3727b40f3b9af6a5763899897a549e743c823bba
dubletab <gnevyshevdv@gmail.com>
2021-09-08 19:51:41 +0300

    add event.target on func

--- app.js
+++ app.js
@@ -10,31 +10,28 @@ for (let i = 0; i < SQUARE_NUMBER; i++){
     const square = document.createElement('div');
     square.classList.add('square');
 
-    square.addEventListener('mouseover', ()=>{
-        setColor(square);
-    })
+    square.addEventListener('mouseover', setColor)
 
-    square.addEventListener('mouseleave', ()=>{
-        removeColor(square)
-    })
+    square.addEventListener('mouseleave', removeColor)
 
     board.append(square);
 }
 
 
-function setColor(elem) {
-    const color = getRandomColor(colors)
+function setColor(event) {
+    const color = getRandomColor(colors);
+    const elem = event.target;
     elem.style.background = color;
     elem.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
 }
 
-function removeColor(elem) {
+function removeColor(event) {
+    const elem = event.target;
     elem.style.background = '#1d1d1d';
     elem.style.boxShadow = `0 0 2px #000`
 }
 
 function getRandomColor(myArr) {
-    const index = Math.floor(Math.random() * colors.length);
-    return myArr[index];
+    return myArr[Math.floor(Math.random() * colors.length)];
 }
 
